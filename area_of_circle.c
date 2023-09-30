ba6b942740f0c41fc5e02cbad224b1fc56bb68bb
MOHAMMAD AMAN KHAN <146350867+ynotaman@users.noreply.github.com>
2023-09-30 22:31:19 +0530

    Add files via upload

---
+++ area_of_circle.c
@@ -0,0 +1,17 @@
+// area of circle 
+#include <stdio.h>
+int main()
+{
+    float  a, r;
+    
+    printf ("radius of the circle r\n");
+    scanf ("%f",&r);
+    
+    a=3.14*r*r; 
+    printf("the area of the circle is %f\n",a);
+    return 0;
+    
+}
+
+
+
